30abe6daa60c87df24bff6efa48d2ec29b570794
KariminK <103779342+KariminK@users.noreply.github.com>
2024-04-17 11:46:38 +0200

    added section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 const Home = () => {
   return (
     <>
-      <h1>Come on and buy best products in cheapest prices</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum neque in
-        itaque assumenda eos doloribus adipisci cupiditate porro vitae iusto.
-      </p>
+      <section className="mx-auto w-1/3 mt-20 flex">
+        <h1 className="text-5xl font-bold">
+          Come on and buy best products in cheapest prices
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum neque
+          in itaque assumenda eos doloribus adipisci cupiditate porro vitae
+          iusto.
+        </p>
+      </section>
     </>
   );
 };
